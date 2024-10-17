a0808ee3011e036fa1adc28b9e143d02171926fd
SaffiUllah1999 <msaffiullah1999smile@gmail.com>
2024-10-17 16:34:53 +0500

    Lazy Loading and mobile responsive

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -2,23 +2,32 @@ import {
   Component,
   ChangeDetectionStrategy,
   ChangeDetectorRef,
+  HostListener,
 } from '@angular/core';
-
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrl: './dashboard.component.scss',
-  changeDetection: ChangeDetectionStrategy.OnPush
-
+  styleUrls: ['./dashboard.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class DashboardComponent {
-
+  isSidebarVisible = true;
 
   constructor() {
-
+    this.updateContent(); // Initial check on load
   }
 
+  @HostListener('window:resize', ['$event'])
+  onResize(event:object) {
+    this.updateContent();
+  }
 
-
+  updateContent() {
+    const width = window.innerWidth;
+    this.isSidebarVisible = width >= 768; // Determine visibility based on width
+  }
+  toggleSidebar() {
+    this.isSidebarVisible = !this.isSidebarVisible; // Toggle sidebar visibility
+  }
 }
